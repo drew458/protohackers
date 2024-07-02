0f873702a61c355bdb89a20274a19ea21fdf85dc
drew458 <43808240+drew458@users.noreply.github.com>
2024-07-02 10:52:21 +0200

    2 updates

--- 2_means_to_end/src/main.rs
+++ 2_means_to_end/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:9876").unwrap();
+
+    // Each connection spawns a new thread
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| handle_connection(stream));
+            }
+            Err(e) => {
+                println!("connection failed. Error: {}", e)
+            }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    
+
 }
