41694aa88836ebb19cb92be5ca84cca2626fcef6
drew458 <43808240+drew458@users.noreply.github.com>
2024-07-02 10:21:49 +0200

    fmt

--- 1_prime_time/src/main.rs
+++ 1_prime_time/src/main.rs
@@ -1,26 +1,26 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct Request {
     method: String,
-    number: i64
+    number: i64,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Response {
     method: String,
-    prime: bool
+    prime: bool,
 }
 
 impl Response {
-
     fn new(method: String, prime: bool) -> Response {
-        Response {
-            method,
-            prime
-        }
+        Response { method, prime }
     }
 }
 
@@ -33,20 +33,19 @@ fn main() {
             Ok(stream) => {
                 thread::spawn(|| handle_connection(stream));
             }
-            Err(e) => { println!("connection failed. Error: {}", e) }
+            Err(e) => {
+                println!("connection failed. Error: {}", e)
+            }
         }
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-
     loop {
-
         let mut total_str = String::new();
 
         // Loop over the single request written by the connection
         loop {
-
             let mut buf = Vec::with_capacity(1);
             stream.read(&mut buf).unwrap();
 
@@ -62,38 +61,33 @@ fn handle_connection(mut stream: TcpStream) {
         let req: Result<Request, serde_json::Error> = serde_json::from_str(&total_str);
         match req {
             Ok(req) => {
-
                 // Whenever you receive a conforming request, send back a correct response, and wait for another request.
                 let mut res = Response::new(req.method, false);
 
-                if is_prime(req.number){
+                if is_prime(req.number) {
                     res.prime = true;
                 }
-                
-                write_res(&stream, &res);
-            },
-            Err(_e) => {
 
+                write_res(&stream, &res);
+            }
+            Err(_e) => {
                 // Whenever you receive a malformed request, send back a single malformed response, and disconnect the client.
                 let res = Response::new("Malformed".into(), false);
                 write_res(&stream, &res);
                 return;
             }
         }
-        
     }
 }
 
 fn write_res<T>(mut stream: &TcpStream, res: &T)
 where
-    T: ?Sized + Serialize, 
+    T: ?Sized + Serialize,
 {
-
-    let mut res_str = serde_json::to_string(res).unwrap()
-            .replace("\n", "");
+    let mut res_str = serde_json::to_string(res).unwrap().replace("\n", "");
     res_str.push_str("\n");
 
-    stream.write_all(res_str.as_bytes()).unwrap(); 
+    stream.write_all(res_str.as_bytes()).unwrap();
 }
 
 fn is_prime(n: i64) -> bool {
