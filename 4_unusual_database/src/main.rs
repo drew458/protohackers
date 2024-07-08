02e9dad799880c2ee3b42d7216e7e495fe940928
drew458 <43808240+drew458@users.noreply.github.com>
2024-07-08 11:44:15 +0200

    4 - add multithread

--- 4_unusual_database/src/main.rs
+++ 4_unusual_database/src/main.rs
@@ -1,4 +1,10 @@
-use std::{collections::HashMap, net::UdpSocket, str::from_utf8, sync::{Arc, RwLock}};
+use std::{
+    collections::HashMap,
+    net::{SocketAddr, UdpSocket},
+    str::from_utf8,
+    sync::{Arc, RwLock},
+    thread,
+};
 
 const ADDR: &str = "0.0.0.0";
 const PORT: u16 = 8888;
@@ -6,27 +12,41 @@ const PORT: u16 = 8888;
 fn main() {
     let socket = UdpSocket::bind(format!("{}:{}", ADDR, PORT)).unwrap();
     println!("Listening on port {PORT}");
+    let _ = socket.set_read_timeout(None); // Read calls will block indefinitely
 
     let db: Arc<RwLock<HashMap<String, String>>> = Arc::from(RwLock::new(HashMap::new()));
 
-    let mut buf = Vec::new();
-    let _ = socket.recv(&mut buf);
+    loop {
+        let mut buf = Vec::new();
+        let (_, src) = socket.recv_from(&mut buf).unwrap();
 
+        let shared_db = db.clone();
+        let socket_clone = socket.try_clone().expect("couldn't clone the socket");
+
+        thread::spawn(move || execute_request(buf, socket_clone, src, shared_db));
+    }
+}
+
+fn execute_request(
+    buf: Vec<u8>,
+    socket: UdpSocket,
+    src: SocketAddr,
+    db: Arc<RwLock<HashMap<String, String>>>,
+) {
     let message = from_utf8(&buf).unwrap();
 
     // Insert
     if message.contains("=") {
-        let parts = message.split_once("=").unwrap();
+        let (key, val) = message.split_once("=").unwrap();
 
-        db.write().unwrap().insert(parts.0.to_string(), parts.1.to_string());
-
-
-    } else {    // Retrieve
+        db.write().unwrap().insert(key.to_string(), val.to_string());
+    } else {
+        // Retrieve
         let value = db.read().unwrap().get(message).cloned().unwrap();
 
         let response_message: &str = &format!("{message}={value}");
 
         let buf: &[u8] = response_message.as_bytes();
-        let _ = socket.send(buf);
+        let _ = socket.send_to(buf, src);
     }
 }
